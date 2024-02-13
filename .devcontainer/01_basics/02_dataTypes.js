301edd92865eec47c91946542f715a2bc161a464
anshulag931 <100944619+anshulag931@users.noreply.github.com>
2024-02-13 18:12:52 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strick";
+//this is use for the code in the new version 
+
+// use code in easily readable i.e readability
+
+
+let names = "anshul"
+let age = 19
+let isValid = true
+let state; // undefined
+let wheater= null
+
+//premitive data types
+/*
+number 
+bigint
+String
+boolean
+null   standalone variable
+undefined 
+*/
+
+console.table([names,age,isValid,state,wheater])
+
